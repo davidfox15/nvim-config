176ab627f362a313d536695610ce15e0856e6055
David Lisitsyn <davidfox15@yandex.ru>
2023-11-07 12:32:21 +0300

    Add keymaps for splits, windows and tabs

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -9,3 +9,28 @@ vim.keymap.set('n', '<C-a>', 'gg<S-v>G')
 
 -- Do not yank with x
 vim.keymap.set('n', 'x', '"_x')
+
+-- clear search highlights
+vim.keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
+
+-- New tab
+vim.keymap.set('n', 'te', ':tabedit<CR>')
+vim.keymap.set('n', 'tt', ':tabedit %:p<CR>')
+vim.keymap.set('n', 'tc', ':tabclose<CR>')
+
+-- Split window
+vim.keymap.set('n', 'ss', ':split<Return><C-w>w')
+vim.keymap.set('n', 'sv', ':vsplit<Return><C-w>w')
+
+-- Move window
+vim.keymap.set('n', '<Space>', '<C-w>w')
+vim.keymap.set('', 'sh', '<C-w>h')
+vim.keymap.set('', 'sk', '<C-w>k')
+vim.keymap.set('', 'sj', '<C-w>j')
+vim.keymap.set('', 'sl', '<C-w>l')
+
+-- Resize window
+vim.keymap.set('n', '<C-w><left>', '<C-w><')
+vim.keymap.set('n', '<C-w><right>', '<C-w>>')
+vim.keymap.set('n', '<C-w><up>', '<C-w>+')
+vim.keymap.set('n', '<C-w><down>', '<C-w>-')
