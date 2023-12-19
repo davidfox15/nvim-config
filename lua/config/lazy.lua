cc2c377ac51bfaad9da9ed1b3ffc4ce66675d75b
David Lisitsyn <davidfox15@yandex.ru>
2023-12-20 00:00:34 +0300

    Delete not used plugins

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -23,6 +23,11 @@ require("lazy").setup({
 		-- Disable plugins
 		{ "echasnovski/mini.bufremove", enabled = false },
 		{ "echasnovski/mini.nvim", enabled = false },
+		{ "echasnovski/mini.surround", enabled = false },
+		{ "echasnovski/mini.ai", enabled = false },
+		{ "echasnovski/mini.animate", enabled = false },
+		{ "echasnovski/mini.comment", enabled = false },
+		{ "echasnovski/mini.pairs", enabled = false },
 		{ "folke/flash.nvim", enabled = false },
 	},
 	defaults = {
--- lua/plugins/nvim-autopairs.lua
+++ lua/plugins/nvim-autopairs.lua
@@ -1,18 +1,18 @@
 return {
-  "windwp/nvim-autopairs",
-  event = "InsertEnter",
-  config = function()
-    local Rule = require("nvim-autopairs.rule")
-    local npairs = require("nvim-autopairs")
+	"windwp/nvim-autopairs",
+	event = "InsertEnter",
+	config = function()
+		-- local Rule = require("nvim-autopairs.rule")
+		-- local npairs = require("nvim-autopairs")
 
-    require("nvim-autopairs").setup({
-      disable_filetype = { "TelescopePrompt", "vim" },
-    })
+		require("nvim-autopairs").setup({
+			disable_filetype = { "TelescopePrompt", "vim" },
+		})
 
-    local cond = require("nvim-autopairs.conds")
+		local cond = require("nvim-autopairs.conds")
 
-    npairs.add_rules({
-      Rule("<", "", "-vim"):with_pair(cond.not_before_regex("/")),
-    })
-  end,
+		-- npairs.add_rules({
+		-- 	Rule("<", "", "-vim"):with_pair(cond.not_before_regex("/")),
+		-- })
+	end,
 }
