8e1e1bebff2a95a29f4d50d7734778a570fdc5d4
David Lisitsyn <davidfox15@yandex.ru>
2024-01-09 13:30:28 +0300

    Add gitsigns

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -1,30 +1,33 @@
 return {
-	"folke/which-key.nvim",
-	event = "VeryLazy",
-	init = function()
-		vim.o.timeout = true
-		vim.o.timeoutlen = 300
-	end,
-	opts = {
-		-- your configuration comes here
-		-- or leave it empty to use the default settings
-		-- refer to the configuration section below
-	},
-	config = function()
-		local wk = require("which-key")
-		wk.register({
-			g = {
-				name = "Git",
-			},
-			f = {
-				name = "Telescope",
-			},
-			c = {
-				name = "Code actions",
-			},
-			b = {
-				name = "Buffer actions",
-			},
-		}, { prefix = "<leader>" })
-	end,
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    init = function()
+        vim.o.timeout = true
+        vim.o.timeoutlen = 300
+    end,
+    opts = {
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+    },
+    config = function()
+        local wk = require("which-key")
+        wk.register({
+            g = {
+                name = "Git",
+            },
+            f = {
+                name = "Telescope",
+            },
+            c = {
+                name = "Code actions",
+            },
+            b = {
+                name = "Buffer actions",
+            },
+            h = {
+                name = "Harpoon / Hunk",
+            },
+        }, { prefix = "<leader>" })
+    end,
 }
