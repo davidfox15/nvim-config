35d6283be0462c7f949d58d7ef658932895e638c
David Lisitsyn <davidfox15@yandex.ru>
2024-01-10 22:34:16 +0300

    Update treesitter config

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,7 +4,7 @@ return {
 	config = function()
 		local config = require("nvim-treesitter.configs")
 		config.setup({
-			ensure_installed = { "lua", "javascript" },
+			ensure_installed = { "lua", "vimdoc", "javascript", "typescript", "tsx" },
 			highlight = { enable = true },
 			indent = { enable = true },
 		})
