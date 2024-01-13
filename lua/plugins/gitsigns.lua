c54b05bff9ee40b275b517c6ba410019aff03092
David Lisitsyn <davidfox15@yandex.ru>
2024-01-14 00:42:11 +0300

    Add gitsigns status show priority

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -2,6 +2,7 @@ return {
 	"lewis6991/gitsigns.nvim",
 	config = function()
 		require("gitsigns").setup({
+			sign_priority = 100,
 			on_attach = function(bufnr)
 				local gs = package.loaded.gitsigns
 
