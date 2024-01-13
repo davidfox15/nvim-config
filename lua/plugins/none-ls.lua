3cc326ac97e9370c804238324ceeb166fc81350e
David Lisitsyn <davidfox15@yandex.ru>
2024-01-14 00:38:45 +0300

    Update linting and formatting

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,19 +1,21 @@
 return {
-    "nvimtools/none-ls.nvim",
-    config = function()
-        local null_ls = require("null-ls")
+	"nvimtools/none-ls.nvim",
+	lazy = true,
+	-- event = { "BufReadPre", "BufNewFile" }, -- to disable, comment this out
+	config = function()
+		local null_ls = require("null-ls")
 
-        null_ls.setup({
-            sources = {
-                null_ls.builtins.formatting.stylua,
-                null_ls.builtins.formatting.prettierd,
-                null_ls.builtins.diagnostics.eslint_d,
-                null_ls.builtins.completion.spell,
-            },
-        })
+		null_ls.setup({
+			sources = {
+				null_ls.builtins.formatting.stylua,
+				null_ls.builtins.formatting.prettierd,
+				null_ls.builtins.diagnostics.eslint_d,
+				null_ls.builtins.completion.spell,
+			},
+		})
 
-        vim.keymap.set("n", "<space>cf", function()
-            vim.lsp.buf.format({ async = true })
-        end, { desc = "Fromat document" })
-    end,
+		vim.keymap.set("n", "<space>cf", function()
+			vim.lsp.buf.format({ async = true })
+		end, { desc = "Fromat document" })
+	end,
 }
