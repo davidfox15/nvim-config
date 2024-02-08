4a2148c0320f032190cf3a3df31f6317ae88bf1e
David Lisitsyn <davidfox15@yandex.ru>
2024-02-08 21:45:33 +0300

    Improve config Add undo dir, improve options, add auto resize

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,6 +1,5 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
 
 -- line numbers
 vim.opt.relativenumber = true -- show relative line numbers
@@ -14,22 +13,26 @@ vim.opt.ignorecase = true
 vim.opt.smartcase = true
 vim.opt.showmatch = true
 vim.opt.incsearch = true
+vim.opt.hlsearch = false
 -- Tab
-vim.opt.shiftwidth = 2
 vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 vim.opt.smartindent = true
 vim.opt.autoindent = true
 -- Clipboard
 vim.opt.clipboard = "unnamedplus"
+-- Work with files
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
 -- Lines
 vim.opt.wrap = false
 -- split windows
 vim.opt.splitright = true -- split vertical window to the right
 vim.opt.splitbelow = true -- split horizontal window to the bottom
-
--- turn off swapfile
-vim.opt.swapfile = false
 
 -- turn on termguicolors for nightfly colorscheme to work
 -- (have to use iterm2 or any other true color terminal)
@@ -39,13 +42,11 @@ vim.opt.signcolumn = "yes" -- show sign column so that text doesn't shift
 
 vim.opt.scrolloff = 4
 
-vim.opt.updatetime = 50
+vim.opt.updatetime = 50 -- auto save timer
 
 vim.opt.termguicolors = true
 
--- recommended settings from nvim-tree documentation
--- vim.g.loaded_netrw = 1
--- vim.g.loaded_netrwPlugin = 1
--- vim.g.netrw_liststyle = 3
-
 vim.opt.list = false
+
+-- colored 80 col
+-- vim.opt.colorcolumn = "80"
