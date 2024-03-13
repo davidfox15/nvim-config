a6d1c2d7b2752f35048779a89e82df50ed2fd0d6
David Lisitsyn <davidfox15@yandex.ru>
2024-03-13 18:39:10 +0300

    Fix autoformat on save toggle

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -36,31 +36,27 @@ return {
 				python = { "isort", "black" },
 				rust = { "rustfmt" },
 			},
-			format_on_save = function(bufnr)
+			format_on_save = function()
 				if vim.g.auto_format then
-					return
+					return { timeout_ms = 500, lsp_fallback = true }
 				end
-				return { timeout_ms = 500, lsp_fallback = true }
 			end,
+		})
+
+		vim.api.nvim_create_user_command("FormatingToggle", function()
+			if vim.g.auto_format then
+				vim.g.auto_format = false
+			else
+				vim.g.auto_format = true
+			end
+			vim.notify("autoformat-on-save " .. tostring(vim.g.auto_format), "info", { title = "conform.nvim" })
+		end, {
+			desc = "Toggle autoformat-on-save",
 		})
 
 		vim.keymap.set("n", "<space>tf", function()
-			if vim.g.auto_format then
-				vim.g.auto_format = false
-				vim.notify("Document format off", "info", { title = "conform.nvim" })
-			else
-				vim.g.auto_format = true
-				vim.notify("Document format on", "info", { title = "conform.nvim" })
-			end
+			vim.cmd("FormatingToggle")
 		end, { desc = "Toggle formatting on save" })
-
-		vim.api.nvim_create_autocmd("BufWritePre", {
-			pattern = "*",
-			callback = function(args)
-				vim.notify("Document format", "info", { title = "conform.nvim" })
-				require("conform").format({ bufnr = args.buf })
-			end,
-		})
 
 		vim.keymap.set("n", "<leader>cf", function()
 			require("conform").format({ async = true, lsp_fallback = true })
