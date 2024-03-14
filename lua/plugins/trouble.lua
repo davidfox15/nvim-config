c29cdd44c4a281b9cafea82db86dad6731fa50e8
David Lisitsyn <davidfox15@yandex.ru>
2024-03-15 00:06:01 +0300

    Improve trouble

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -5,6 +5,7 @@ return {
     { "<leader>xx", "<cmd>TroubleToggle<cr>",                       desc = "Trouble" },
     { "<leader>xw", "<cmd>TroubleToggle workspace_diagnostics<cr>", desc = "Trouble workspace diagnostics" },
     { "<leader>xd", "<cmd>TroubleToggle document_diagnostics<cr>",  desc = "Trouble document diagnostics" },
+    { "<leader>xq", "<cmd>TroubleToggle quickfix<cr>",  desc = "Trouble quickfix" },
   },
   opts = {
     action_keys = {
