92d219040b6c1ef942af45463f1d500f9d478932
David Lisitsyn <davidfox15@yandex.ru>
2024-03-23 23:34:04 +0300

    fix expandtab

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -22,7 +22,7 @@ vim.opt.hlsearch = true
 vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
-vim.opt.expandtab = true
+-- vim.opt.expandtab = true
 vim.opt.smartindent = true
 vim.opt.autoindent = true
 -- Clipboard
