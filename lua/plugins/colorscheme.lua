fe7240a32afd49a67eaa5a0d709ce19bc89dbf5f
David Lisitsyn <davidfox15@yandex.ru>
2024-03-23 23:34:45 +0300

    Fix colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,28 +1,28 @@
 return {
 	{
 		"catppuccin/nvim",
-		enabled = false,
+		enabled = true,
 		lazy = false,
 		name = "catppuccin",
 		priority = 1000,
 		config = function()
-			local colors = require("catppuccin.palettes").get_palette()
-			colors.none = "NONE"
-			require("catppuccin").setup({
-				flavour = "frappe",
-				transparent_background = true,
-				custom_highlights = {
-					Comment = { fg = colors.overlay1 },
-					LineNr = { fg = colors.overlay1 },
-					CursorLine = { bg = colors.none },
-					CursorLineNr = { fg = colors.lavender },
-					DiagnosticVirtualTextError = { bg = colors.none },
-					DiagnosticVirtualTextWarn = { bg = colors.none },
-					DiagnosticVirtualTextInfo = { bg = colors.none },
-					DiagnosticVirtualTextHint = { bg = colors.none },
-				},
-			})
-			vim.cmd.colorscheme("catppuccin")
+			-- local colors = require("catppuccin.palettes").get_palette()
+			-- colors.none = "NONE"
+			-- require("catppuccin").setup({
+			-- 	flavour = "frappe",
+			-- 	transparent_background = true,
+			-- 	custom_highlights = {
+			-- 		Comment = { fg = colors.overlay1 },
+			-- 		LineNr = { fg = colors.overlay1 },
+			-- 		CursorLine = { bg = colors.none },
+			-- 		CursorLineNr = { fg = colors.lavender },
+			-- 		DiagnosticVirtualTextError = { bg = colors.none },
+			-- 		DiagnosticVirtualTextWarn = { bg = colors.none },
+			-- 		DiagnosticVirtualTextInfo = { bg = colors.none },
+			-- 		DiagnosticVirtualTextHint = { bg = colors.none },
+			-- 	},
+			-- })
+			vim.cmd.colorscheme("catppuccin-macchiato")
 		end,
 	},
 	{
@@ -40,12 +40,12 @@ return {
 	},
 	{
 		"folke/tokyonight.nvim",
-		enabled = true,
+		enabled = false,
 		lazy = false,
 		priority = 1000,
 		opts = {},
 		config = function()
-			vim.cmd.colorscheme("tokyonight")
+			vim.cmd.colorscheme("tokyonight-night")
 		end,
 	},
 }
