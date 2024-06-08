cd3bd278be9eadb96c52b7df26ce41d41d854f34
David Lisitsyn <davidfox15@yandex.ru>
2024-06-08 12:31:49 +0300

    Setup trouble plugin

--- lua/fuchsdog/plugins/trouble.lua
+++ lua/fuchsdog/plugins/trouble.lua
@@ -19,14 +19,30 @@ return {
 			desc = "Buffer Diagnostics (Trouble)",
 		},
 		{
-			"<leader>xq",
+			"<leader>xf",
 			"<cmd>Trouble qflist toggle<cr>",
 			desc = "Quickfix List (Trouble)",
 		},
+		{
+			"gt",
+			"<cmd>Trouble lsp_type_definitions togg<cr>",
+			desc = "Type defenition (Trouble)",
+		},
 		{ "<leader>xt", "<cmd>TodoTrouble<cr>", desc = "Trouble Todo" },
+		{
+			"<leader>xq",
+			"<cmd>Trouble close<cr>",
+			desc = "Close (Trouble)",
+		},
 		-- { "gr", "<cmd>TroubleToggle lsp_references<cr>", desc = "Trouble lsp_references" },
 		-- 		{ "<lead>xw", "<cmd>TroubleToggle workspace_diagnostics<cr>", desc = "Trouble workspace diagnostics" },
 		-- 		{ "<leader>xd", "<cmd>TroubleToggle document_diagnostics<cr>", desc = "Trouble document diagnostics" },
 		-- 		{ "<leader>xq", "<cmd>TroubleToggle quickfix<cr>", desc = "Trouble quickfix" },
 	},
+	config = function()
+		require("trouble").setup({
+			-- auto_close = true, -- auto close when there are no items
+			focus = true, -- Focus the window when opened
+		})
+	end,
 }
