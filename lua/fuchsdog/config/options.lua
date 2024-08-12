e9d0f0218291f670ad08c50e3f94fefa85f84b6d
Давид Лисицын <davidfox15@yandex.ru>
2024-08-12 23:35:45 +0300

    Change tabs indent to 4

--- lua/fuchsdog/config/options.lua
+++ lua/fuchsdog/config/options.lua
@@ -20,13 +20,11 @@ vim.opt.showmatch = true
 vim.opt.incsearch = true
 vim.opt.hlsearch = true
 -- Tab
-vim.opt.tabstop = 2
-vim.opt.softtabstop = 2
-vim.opt.shiftwidth = 2
--- Change tab to spaces
--- vim.opt.expandtab = true
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
 vim.opt.smartindent = true
-vim.opt.autoindent = true
 -- Clipboard
 vim.opt.clipboard = "unnamedplus"
 -- Work with files
