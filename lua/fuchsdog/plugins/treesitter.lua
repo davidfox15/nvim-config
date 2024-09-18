f657f70c5cee2b813138307167e706b111db0729
Давид Лисицын <davidfox15@yandex.ru>
2024-09-18 21:55:39 +0300

    Update treesitter config

--- lua/fuchsdog/plugins/treesitter.lua
+++ lua/fuchsdog/plugins/treesitter.lua
@@ -2,64 +2,72 @@ return {
 	{
 		"nvim-treesitter/nvim-treesitter",
 		build = ":TSUpdate",
-		config = function()
-			local config = require("nvim-treesitter.configs")
-			config.setup({
-				ensure_installed = {
-					"lua",
-					"vimdoc",
-					"javascript",
-					"typescript",
-					"tsx",
-					"html",
-					"scss",
-					"css",
-					"json",
-					"markdown",
-					"markdown_inline",
-					"gitignore",
-					"bash",
-					"rust",
-					"c",
-				},
+		main = "nvim-treesitter.configs", -- main module to put in config, require(main).setup(opts)
+		opts = {
+			ensure_installed = {
+				"lua",
+				"vim",
+				"vimdoc",
+				"javascript",
+				"typescript",
+				"tsx",
+				"html",
+				"scss",
+				"css",
+				"json",
+				"regex",
+				"bash",
+				"sql",
+				"dockerfile",
+				"gitignore",
+				"yaml",
+				"graphql",
+				"go",
+				"markdown",
+				"markdown_inline",
+				"rust",
+				"c",
+			},
+			auto_install = true,
+			highlight = {
+				enable = true,
+				-- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
+				--  If you are experiencing weird indenting issues, add the language to
+				--  the list of additional_vim_regex_highlighting and disabled languages for indent.
+				additional_vim_regex_highlighting = { "ruby" },
 
-				highlight = {
-					enable = true,
-					-- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-					-- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-					-- Using this option may slow down your editor, and you may see some duplicate highlights.
-					-- Instead of true it can also be a list of languages
-					additional_vim_regex_highlighting = false,
-					disable = function(_, bufnr)
-						local buf_name = vim.api.nvim_buf_get_name(bufnr)
-						local file_size = vim.api.nvim_call_function("getfsize", { buf_name })
-						return file_size > 256 * 1024
-					end,
-				},
+				-- TODO: How to open big files ???
+				--
+				-- XOde for off treesitter in big files
+				-- disable = function(_, bufnr)
+				-- 	local buf_name = vim.api.nvim_buf_get_name(bufnr)
+				-- 	local file_size = vim.api.nvim_call_function("getfsize", { buf_name })
+				-- 	return file_size > 256 * 1024
+				-- end,
+			},
+			indent = { enable = true, disable = { "ruby" } },
 
-				indent = { enable = true },
-
-				-- Visual by treesitter elements
-				incremental_selection = {
-					enable = true,
-					keymaps = {
-						init_selection = "gnn", -- Начать визуальное выделение по контексту
-						node_incremental = "grn", -- Расширить выделение
-						node_decremental = "grm", -- Уменьшить выделение
-						scope_incremental = "grc", -- Расширить выделение до области
-					},
+			-- TODO: Setup treesitter incremental_selection
+			incremental_selection = {
+				enable = true,
+				keymaps = {
+					init_selection = "tn", -- Начать визуальное выделение по контексту
+					node_incremental = "tn", -- Расширить выделение
+					scope_incremental = "ts", -- Расширить выделение до области
+					node_decremental = "tm", -- Уменьшить выделение
 				},
-			})
-		end,
+			},
+		},
 	},
 	{
 		"nvim-treesitter/nvim-treesitter-context",
 		dependencies = { "nvim-treesitter/nvim-treesitter" },
-		config = function()
-			local config = require("treesitter-context")
-			config.setup({
-				max_lines = 5,
-			})
+		opts = {
+			max_lines = 5,
+		},
+		config = function(_, opts)
+			require("treesitter-context").setup(opts)
+
 			vim.keymap.set("n", "gc", function()
 				require("treesitter-context").go_to_context(vim.v.count1)
 			end, { silent = true, desc = "Go to context" })
