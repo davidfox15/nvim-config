7947bdf16d930de5ffe426f4119a53e872d96545
David Lisitsyn <davidfox15@yandex.ru>
2025-04-11 09:17:31 +0300

    Add fugitive

---
+++ lua/fuchsdog/plugins/fugitive.lua
@@ -0,0 +1,8 @@
+return {
+	"tpope/vim-fugitive",
+	lazy = false,
+	config = function()
+		-- Необязательно: дополнительные настройки, если нужны
+		-- vim.cmd("command! -nargs=0 G :Git") -- Пример команды для быстрого доступа
+	end,
+}
